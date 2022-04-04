118571a340b5d9118855bd28b36643ecbdbffb4e
mojonapower <maca95@gmail.com>
2022-04-04 17:54:02 +0000

    done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let button = document.querySelector("button");
+  let mensaje = document.createElement("h1");
+  // let flor = { nombre: "gerbera", color: "rojo", detalle: [1, 2, 3, 4] };
+  // let p = document.createElement("p");
+  // p.innerHTML = `la flor se llama ${flor.nombre}, su color es ${flor.color} ${flor.detalle}`;
+  document.body.appendChild(p);
+  button.addEventListener("click", function() {
+    let edad = document.querySelector("input").value;
+    if (edad >= 18) {
+      mensaje.innerHTML = `tienes ${edad} entonces ya puedes beber`;
+    } else if (edad < 0) {
+      mensaje.innerHTML = "¿cómo puedes escribir si ni siquiera estás vivo?";
+    } else {
+      mensaje.innerHTML =
+        "tienes" + " " + edad + " " + "entonces no puedes beber";
+    }
+    // mensaje.innerHTML = ` ${
+    //   edad < 18
+    //     ? `tienes ${edad}, entonces eres menor de edad`
+    //     : `tienes ${edad}, entonces eres mayor de edad`
+    // } `;
+    // mensaje.innerHTML = `${edad && `tienes ${edad}`}`;
+    //mensaje.innerHTML = `${edad || "edad no especificada"}`;
+
+    document.body.appendChild(mensaje);
+  });
 };
